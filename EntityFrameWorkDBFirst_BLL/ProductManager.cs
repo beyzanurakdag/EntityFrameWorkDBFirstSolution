b597b16b418ee8684ab64c863059cc1e2140be41
103SABAH_BEYZANUR <103SABAH_BEYZANUR@DESKTOP-TUMHS1A>
2021-12-27 09:32:36 +0300

    Add project files.

---
+++ EntityFrameWorkDBFirst_BLL/ProductManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFrameWorkDBFirst_BLL.ViewModels;
+using EntityFrameWorkDBFirst_DAL;
+
+namespace EntityFrameWorkDBFirst_BLL
+{
+    public class ProductManager
+    {
+        NORTHWNDEntities myDBContext = new NORTHWNDEntities();
+        public List<ProductViewModel> TumUrunleriGetir()
+        {
+            List<ProductViewModel> urunler = new List<ProductViewModel>();
+            var data = myDBContext.Products.ToList();
+            foreach (var item in data)
+            {
+                ProductViewModel p = new ProductViewModel()
+                {
+                    CategoryID=item.CategoryID,
+                    ProductID=item.ProductID,
+                    UnitPrice=item.UnitPrice,
+                    UnitsInStock=item.UnitsInStock,
+                    Discontinued=item.Discontinued,
+                    UnitsOnOrder=item.UnitsOnOrder,
+                    ReorderLevel=item.ReorderLevel,
+                    SupplierID=item.SupplierID,
+                    ProductName=item.ProductName,
+                    QuantityPerUnit=item.QuantityPerUnit
+                };
+                //CategoryName
+                //1.yol
+                //p.CategoryName = item.Category.CategoryName;
+                //2.yol
+                p.CategoryName = myDBContext.Categories.FirstOrDefault(x => x.CategoryID == item.CategoryID)?.CategoryName;
+
+                urunler.Add(p);
+            }
+            return urunler;
+        }
+        public bool YeniUrunEkle(ProductViewModel p)
+        {
+            bool sonuc = false;
+            Product newProduct = new Product
+            {
+                ProductName = p.ProductName,
+                Discontinued = p.Discontinued,
+                CategoryID = p.CategoryID
+            };
+
+            myDBContext.Products.Add(newProduct);
+            int affectedRows = myDBContext.SaveChanges();
+            if (affectedRows > 0)
+            {
+                sonuc = true;
+            }
+
+            return sonuc;
+        }
+    }
+}
---
+++ EntityFrameWorkDBFirst_WinFormUI/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EntityFrameWorkDBFirst_BLL;
+using EntityFrameWorkDBFirst_BLL.ViewModels;
+
+namespace EntityFrameWorkDBFirst_WinFormUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ProductManager myProductManager = new ProductManager();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //from yüklenirken grid'e product bilgileri gelsin.
+            GrideProductlariGetir();
+        }
+
+        private void GrideProductlariGetir()
+        {
+            //BLL aracığıyla listemiz gelecek.
+            dataGridView1.DataSource=myProductManager.TumUrunleriGetir();
+            dataGridView1.Columns["SupplierID"].Visible = false;
+            dataGridView1.Columns["CategoryID"].Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Deneme amaçlı yazıyoruz.
+            ProductViewModel yeniUrun = new ProductViewModel()
+            {
+                ProductName="Deneme Ürün",
+                Discontinued=false,
+                CategoryID=1
+            };
+            //myProductManager.YeniUrunEkle(yeniUrun);
+        }
+    }
+}
